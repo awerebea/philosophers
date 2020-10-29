29ccc7183b06542dc8b72a25ea0c33b489db5524
awerebea <awerebea.21@gmail.com>
2020-10-29 15:40:07 +0300

    Remove unnecessary brackets

--- philo_one/src/tools/get_time_in_ms.c
+++ philo_one/src/tools/get_time_in_ms.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 18:07:00 by awerebea          #+#    #+#             */
-/*   Updated: 2020/10/28 23:00:44 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/10/29 15:37:05 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ unsigned long		get_time_in_ms(void)
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return (((time.tv_sec) * 1000) + ((time.tv_usec) / 1000));
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
