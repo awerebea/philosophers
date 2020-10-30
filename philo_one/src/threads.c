28992a43deb9393773abf95b07cac23f4e6b3da7
awerebea <awerebea.21@gmail.com>
2020-10-30 14:39:11 +0300

    Add error checking to system threads functions
    small fix in ft_putstr_fd

--- philo_one/src/threads.c
+++ philo_one/src/threads.c
@@ -6,19 +6,18 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 23:47:32 by awerebea          #+#    #+#             */
-/*   Updated: 2020/10/29 11:50:39 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/10/30 15:23:33 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdlib.h>
 
-static int		init_ph(t_data *data, t_ph *ph, int i)
+static void		init_ph(t_data *data, t_ph *ph, int i)
 {
 	ph->id = i + 1;
 	ph->times_to_eat = data->times_to_eat;
 	ph->data = data;
-	return (0);
 }
 
 int				start_threads(t_data *data)
@@ -27,23 +26,23 @@ int				start_threads(t_data *data)
 	pthread_t	*threads;
 	int			i;
 
-	i = 0;
-	if (!(ph = (t_ph*)malloc(sizeof(t_ph) * data->num_of_ph)))
-		return (ret_msg("error: malloc fails\n", 1));
-	if (!(threads = (pthread_t*)malloc(sizeof(pthread_t) * data->num_of_ph)))
-		return (ret_msg("error: malloc fails\n", 1));
-	while (i < data->num_of_ph)
+	i = -1;
+	if (!(ph = (t_ph*)malloc(sizeof(t_ph) * data->num_of_ph)) \
+		|| !(threads = (pthread_t*)malloc(sizeof(pthread_t) * data->num_of_ph)))
+		return (1);
+	while (++i < data->num_of_ph)
 	{
 		init_ph(data, &ph[i], i);
-		pthread_create(&threads[i], NULL, simulation, &ph[i]);
-		i++;
+		if (pthread_create(&threads[i], NULL, simulation, &ph[i]))
+			return (1);
 	}
-	i = 0;
-	while (i < data->num_of_ph)
+	i = -1;
+	while (++i < data->num_of_ph)
 	{
-		pthread_join(threads[i], NULL);
-		pthread_mutex_destroy(&data->mtx_forks[i]);
-		i++;
+		if (pthread_join(threads[i], NULL))
+			return (1);
+		if (pthread_mutex_destroy(&data->mtx_forks[i]))
+			return (1);
 	}
 	free(ph);
 	free(threads);
--- philo_one/src/tools/ft_putstr_fd.c
+++ philo_one/src/tools/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 12:42:35 by awerebea          #+#    #+#             */
-/*   Updated: 2020/10/27 14:45:40 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/10/30 15:48:49 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	if (NULL == s)
+	if (!s)
 		return ;
 	while (*s)
 		ft_putchar_fd(*s++, fd);
